7791b276df2300d1d98ac69e1041b8a3b4147dfa
Ethan Soutar-Rau <ethan.soutar.rau@gmail.com>
2011-12-15 22:14:31 -0800

    new part that says si

---
+++ spec/mg_hotdog/parts/si_part_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+require 'mg_hotdog/parts/si_part.rb'
+
+describe SiPart do
+  before :each do
+    mock_message_and_robot
+  end
+
+  it 'should repeat "si"' do
+    @message.stub(:body).and_return('si')
+    @robot.should_receive(:speak).with(/si/)
+    SiPart.new.process(@message,@robot)
+  end
+
+  it "shouldn't repeat itself" do
+    @message.stub(:body).and_return('si')
+    @message.stub(:user).and_return('MG HOTDOG')
+
+    @robot.should_not_receive(:speak)
+
+    SiPart.new.process(@message, @robot)
+  end
+end
